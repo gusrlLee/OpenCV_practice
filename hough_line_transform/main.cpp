3eaaf4fcb9d468632cdbb8ae646f52f09b80ba45
gusrlLee <gusrl3204@naver.com>
2022-01-05 11:49:03 +0900

    success tutorial

--- hough_line_transform/main.cpp
+++ hough_line_transform/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using namespace cv;
 
 int main(){
-    Mat dst;
+    Mat dst, cdst, cdstP;
     vector<Vec2f> lines;
 
     Mat image = imread("../building.jpg");
@@ -16,16 +16,15 @@ int main(){
         exit(1);
     }
     resize(image, image, Size(720, 480));
-    cvtColor(image, image, COLOR_BGR2GRAY);
 
-    Canny(image, dst, 100, 255);
+    // cvtColor(image, dst, COLOR_BGR2GRAY);
 
-    HoughLines(dst, lines, 1, CV_PI / 180, 150, 0, 0);
+    Canny(image, cdst, 120, 250, 3);
 
+    HoughLines(cdst, lines, 1, CV_PI / 150, 150, 0, 0);
     for(int i=0; i<lines.size(); i++){
         float rho = lines[i][0], theta = lines[i][1];
         Point pt1, pt2;
-        printf("%d : %f %f \n ", i, rho, theta);
 
         double a = cos(theta);
         double b = sin(theta);
@@ -33,9 +32,18 @@ int main(){
         double x0 = a * rho;
         double y0 = b * rho;
 
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+
+        line(cdst, pt1, pt2, Scalar(255, 255, 255), 3, LINE_AA);
     }
 
-    imshow("image", dst);
+    imshow("image", image);
+    // imshow("dst", dst);
+    imshow("cdst", cdst);
     waitKey();
     destroyAllWindows();
 
